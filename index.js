e6b074562dcaa60483fa5aff554d5a822a50cfec
agsulimko <125898308+agsulimko@users.noreply.github.com>
2023-11-15 09:34:29 +0100

    s

--- index.js
+++ index.js
@@ -50,3 +50,15 @@ invokeAction(options);
 // invokeAction({ action: "remove", id: "Z5sbDlS7pCzNsnAHLtDJd"});
 
 // invokeAction({action: "updateById",id:"Z5sbDlS7pCzNsnAHLtDJd", name:"artem",email: "a.g.sulimko@gmail.com", phone:"0506078888"});
+
+// # Получаем и выводим весь список контактов в виде таблицы (console.table)
+// node index.js --action list
+
+// # Получаем контакт по id - выводим в консоль объект контакта или null, если контакта с таким id не существует.
+// node index.js --action get --id 05olLMgyVQdWRwgKfg5J6
+
+// # Добавляем контакт и выводим в консоль созданный контакт
+// node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+
+// # Удаляем контакт и выводим в консоль удаленный контакт или null, если контакта с таким id не существует.
+// node index.js --action remove --id 5bsw4KVffkAGgQuk0Uk7i
